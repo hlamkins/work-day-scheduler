c0e9dcac44b12232a798dce526a9414de77699ad
Heather Lamkins <lamkinsheather@gmail.com>
2020-11-01 16:48:20 -0600

    Completed Read me and finishing touches

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# work-day-sheduler
+# Work Day Scheduler
+
+## Overview
+A work day planner for a busy schedule. The current hour time block will turn up in red, the future green, and the past grey.
+When the save button is clicked the tasks will be saved in the local storage for future reference. 
+
+## Screenshot
+![Work Day Scheduler](image/worday.png)
+
+## Link to Live Page
+[Work Day Scheduler](https://hlamkins.github.io/work-day-scheduler/)
+
+## Project Status
+This project is complete and functioning. 
--- script.js
+++ script.js
@@ -1,4 +1,54 @@
-let today = document.querySelector("#currentDay");
-let momentEl =moment().format("dddd, MMMM, YYYY");
-
-today.innerHTML = (momentEl);
+$(document).ready(function() {
+    // listen for save button clicks
+    $(".saveBtn").on("click", function() {
+      // get nearby values
+      let value = $(this).siblings(".description").val();
+      let time = $(this).parent().attr("id");
+  
+      // save in localStorage
+      localStorage.setItem(time, value);
+    });
+  
+    function hourUpdater() {
+      // get current number of hours
+      let currentHour = moment().hours();
+  
+      // loop over time blocks
+      $(".time-block").each(function() {
+        let blockHour = parseInt($(this).attr("id").split("-")[1]);
+  
+        // check if we've moved past this time
+        if (blockHour < currentHour) {
+          $(this).addClass("past");
+        } 
+        else if (blockHour === currentHour) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+        } 
+        else {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        }
+      });
+    }
+  
+    hourUpdater();
+  
+    // set up interval to check if current time needs to be updated
+    let interval = setInterval(hourUpdater, 15000);
+  
+    // load any saved data from localStorage
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  
+    
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+  });
